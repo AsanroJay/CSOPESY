1cd3ef4e279d66dcc533541e5214d603b95f4f28
Andre Llanes <andrellanes01@gmail.com>
2026-05-24 21:09:42 +0800

    feat: asci art

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main() {
+        std::printf("  \x1b[1m\x1b[36m ▄████▄  ▄████▄   ▄█████▄  ▀█████▄  ██▀▀▀▀██  ▄████▄  ██      ██ \x1b[0m\n");
+        std::printf("  \x1b[1m\x1b[33m███   ██ ██       ██   ██   ██   ██ ██        ██       ██    ██    \x1b[0m\n");
+        std::printf("  \x1b[1m\x1b[37m██        ▀███▄   ██   ██   ██   ██ ██▀▀▀▀     ▀███▄   ▀██  ██▀    \x1b[0m\n");
+        std::printf("  \x1b[1m\x1b[37m██            ██  ██   ██   █████   ██             ██    ▀██▀      \x1b[0m\n");
+        std::printf("  \x1b[1m\x1b[33m██▄   ██ ▄    ██  ██   ██   ██      ██        ▄    ██     ██     \x1b[0m\n");
+        std::printf("  \x1b[1m\x1b[36m ▀█████▀  ▀███▀   ▀█████▀  ▄██▄     ██▄▄▄▄██   ▀███▀      ██       \x1b[0m\n");
+        std::printf("\n");
+    return 0;
+}
+ 
