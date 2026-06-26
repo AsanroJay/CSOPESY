07b4479569288301ae7ace6da2aec75437d15c26
Jmoney <67088606+AsanroJay@users.noreply.github.com>
2026-06-26 21:16:01 +0800

    fix: undo previous commit changes

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -39,8 +39,8 @@ void FCFSScheduler::startGeneration() {
 
 void FCFSScheduler::stopGeneration() {
     isGenerating.store(false);
-    std::lock_guard<std::mutex> lock(queueMutex);
-    while (!readyQueue.empty()) readyQueue.pop();
+    // std::lock_guard<std::mutex> lock(queueMutex);
+    // while (!readyQueue.empty()) readyQueue.pop();
 }
 
 void FCFSScheduler::shutdown() {
